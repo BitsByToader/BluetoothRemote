8a1fcff522ffb3ffde8ce98ad14a84378aecb949
Ifrim Tudor <ifirmtudor@protonmail.com>
2020-02-18 21:42:16 +0200

    Got networking working with a nice library I found on GitHub. The experience is top notch when the library is made for swift 5.

--- BluetoothRemote/ViewController.swift
+++ BluetoothRemote/ViewController.swift
@@ -7,54 +7,35 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
+    
+    var broadcastConnection: UDPBroadcastConnection!
+    
     //MARK: Properties
     @IBAction func testUDP(_ sender: UIButton) {
         print("pressed")
-//        var addr = in_addr()
-//        inet_pton(AF_INET, "192.168.0.1", &buf)
-        let INADDR_ANY = in_addr(s_addr: 0)
-        udpSend(textToSend: "H", address: INADDR_ANY, port: 5005)
+        do {
+            try broadcastConnection.sendBroadcast("P")
+        } catch {
+            print("error sending")
+        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
-    
-    func udpSend(textToSend: String, address: in_addr, port: CUnsignedShort) {
-
-
-    func htons(value: CUnsignedShort) -> CUnsignedShort {
-        return (value << 8) + (value >> 8);
-    }
-
-    let fd = socket(AF_INET, SOCK_DGRAM, 0) // DGRAM makes it UDP
-
-    let addr = sockaddr_in(
-        sin_len:    __uint8_t(MemoryLayout<sockaddr_in>.size),
-        sin_family: sa_family_t(AF_INET),
-        sin_port:   htons(value: port),
-        sin_addr:   address,
-        sin_zero:   ( 0, 0, 0, 0, 0, 0, 0, 0 )
-    )
-
-        _ = textToSend.withCString { cstr -> Int in
-
-        var localCopy = addr
-
-        let sent = withUnsafePointer(to: &localCopy) { pointer -> Int in
-            let memory = UnsafeRawPointer(pointer).bindMemory(to: sockaddr.self, capacity: 1)
-            let sent = sendto(fd, cstr, strlen(cstr), 0, memory, socklen_t(addr.sin_len))
-            return sent
+        do { broadcastConnection = try UDPBroadcastConnection(
+        port: 5005,
+        handler: { [weak self] (ipAddress: String, port: Int, response: Data) -> Void in
+            print("Received")
+        },
+        errorHandler: { (error) in
+          print(error)
+        })} catch {
+            print("error setting up broadcast connection")
         }
-
-        return sent
-    }
-
-    close(fd)
-
     }
 }
 
