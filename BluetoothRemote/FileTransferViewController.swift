22570e83dbf8e31f57afeefa2533b4a9bae8b367
Ifrim Tudor <ifirmtudor@protonmail.com>
2020-06-01 17:15:57 +0300

    Update commit.

--- BluetoothRemote/FileTransferViewController.swift
+++ BluetoothRemote/FileTransferViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Network
 
 class MyDocument: UIDocument {
     var data: Data?
@@ -32,14 +33,18 @@ class FileTransferViewController: UIViewController, UIDocumentPickerDelegate {
     //MARK: Variables
     let importMenu: UIDocumentPickerViewController = UIDocumentPickerViewController(documentTypes: [String("public.data")], in: .open)
     
-    var fileBroadCastConnection: UDPBroadcastConnection!
+    //var fileBroadCastConnection: UDPBroadcastConnection!
     
     var udpServer: UDPServer? = nil
     var receveingFile: Bool = false
-    var bytesReceived: Int = 0
+    //var bytesReceived: Int = 0
     var receivedFile: Data = Data()
     var fileSizeInBytes: Int = 0
     var receivedFileName: String?
+    
+    var connection: NWConnection?
+    var hostUDP: NWEndpoint.Host = "255.255.255.255"
+    var portUDP: NWEndpoint.Port = 9093
     
     //MARK: Outlets
     
@@ -59,11 +64,12 @@ class FileTransferViewController: UIViewController, UIDocumentPickerDelegate {
         
         if ( sender.isOn ) {
             print("server on")
-            udpServer = UDPServer(address: "255.255.255.255", port: 5006)
-            receivedFile = Data()
-            DispatchQueue.global(qos: .background).async {
-                self.receiveData()
-            }
+            connectToUDP(hostUDP, portUDP)
+            //udpServer = UDPServer(address: "255.255.255.255", port: 5006)
+            //receivedFile = Data()
+            //DispatchQueue.global(qos: .background).async {
+            //    self.receiveData()
+            //}
         } else {
             print("server off")
             udpServer?.close()
@@ -173,10 +179,10 @@ class FileTransferViewController: UIViewController, UIDocumentPickerDelegate {
     }
     
     func receiveData() {
-        let (buff, _, _) = udpServer?.recv(1000) ?? ([0x0], "", 1)
-        if ( buff != nil ) {
+        let (receivingUDPBuffer, _, _) = udpServer?.recv(1000) ?? ([0x0], "", 1)
+        if ( receivingUDPBuffer != nil ) {
             if ( !receveingFile ) {
-                let packet = String(bytes: buff ?? [], encoding: String.Encoding.utf8)
+                let packet = String(bytes: receivingUDPBuffer ?? [], encoding: String.Encoding.utf8)
                 let fileDescription = packet?.components(separatedBy: "/")
                 
                 receivedFileName = (fileDescription ?? ["", ""])[0]
@@ -184,15 +190,17 @@ class FileTransferViewController: UIViewController, UIDocumentPickerDelegate {
                 
                 print("\(String(describing: receivedFileName)) -> \(fileSizeInBytes)")
                 
+                receivedFile = Data(capacity: fileSizeInBytes)
+                
                 receveingFile = true
                 
                 receiveData()
                 return
             } else if ( receveingFile ) {
-                print(buff as Any)
-                receivedFile += buff ?? [Byte()]
+                print(receivingUDPBuffer as Any)
+                receivedFile += receivingUDPBuffer ?? [Byte()]
                 print(receivedFile.count)
-                if ( receivedFile.count == fileSizeInBytes ) {
+                if ( receivedFile.count == fileSizeInBytes || receivedFile.count > fileSizeInBytes ) {
                     receveingFile = false
                     
                     DispatchQueue.main.async {
@@ -211,6 +219,7 @@ class FileTransferViewController: UIViewController, UIDocumentPickerDelegate {
                     return
                 } else if ( receivedFile.count < fileSizeInBytes) {
                     receiveData()
+                    return
                 }
             }
         }
@@ -255,4 +264,70 @@ class FileTransferViewController: UIViewController, UIDocumentPickerDelegate {
             print("error setting up broadcast connection")
         }
     }*/
+    
+    
+    
+    
+    
+    func connectToUDP(_ hostUDP: NWEndpoint.Host, _ portUDP: NWEndpoint.Port) {
+        // Transmited message:
+        let messageToUDP = "Test message"
+
+        self.connection = NWConnection(host: hostUDP, port: portUDP, using: .udp)
+        
+        self.connection?.stateUpdateHandler = { (newState) in
+            print("This is stateUpdateHandler:")
+            switch (newState) {
+                case .ready:
+                    print("State: Ready\n")
+                    self.sendUDP(messageToUDP)
+                    self.receiveUDP()
+                case .setup:
+                    print("State: Setup\n")
+                case .cancelled:
+                    print("State: Cancelled\n")
+                case .preparing:
+                    print("State: Preparing\n")
+                default:
+                    print("ERROR! State not defined!\n")
+            }
+        }
+
+        self.connection?.start(queue: .global())
+    }
+
+    /*func sendUDP(_ content: Data) {
+        self.connection?.send(content: content, completion: NWConnection.SendCompletion.contentProcessed(({ (NWError) in
+            if (NWError == nil) {
+                print("Data was sent to UDP")
+            } else {
+                print("ERROR! Error when data (Type: Data) sending. NWError: \n \(NWError!)")
+            }
+        })))
+    }*/
+
+    func sendUDP(_ content: String) {
+        let contentToSendUDP = content.data(using: String.Encoding.utf8)
+        self.connection?.send(content: contentToSendUDP, completion: NWConnection.SendCompletion.contentProcessed(({ (NWError) in
+            if (NWError == nil) {
+                print("Data was sent to UDP")
+            } else {
+                print("ERROR! Error when data (Type: Data) sending. NWError: \n \(NWError!)")
+            }
+        })))
+    }
+
+    func receiveUDP() {
+        self.connection?.receiveMessage { (data, context, isComplete, error) in
+            if (isComplete) {
+                print("Receive is complete")
+                if (data != nil) {
+                    let backToString = String(decoding: data!, as: UTF8.self)
+                    print("Received message: \(backToString)")
+                } else {
+                    print("Data == nil")
+                }
+            }
+        }
+    }
 }
